2ff129c28e45b9bf197312f4ebba17aa4a161758
arturavakyan <152524925+arturavakyan@users.noreply.github.com>
2023-12-01 23:11:08 +0100

    Update dark_mode.js

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -5,14 +5,23 @@ document.addEventListener('DOMContentLoaded', function() {
     mode_toggle.addEventListener("click", function() {
         toggleTheme(localStorage.getItem("theme"));
     theme=localStorage.getItem("theme");
-    if (theme == "dark") {
-      // Dark mode is active
-      googleMapsIframe.style.filter = 'grayscale(1) invert(1) hue-rotate(180deg)';
-      AdobeDCview.style.filter = 'grayscale(1) invert(1) hue-rotate(180deg)';
-    } else {
-      // Dark mode is inactive
-      googleMapsIframe.style.filter = 'none'; // Reset to default
-      AdobeDCview.style.filter = 'none'; // Reset to default
+    if (googleMapsIframe !== null){
+        if (theme == "dark") {
+          // Dark mode is active
+          googleMapsIframe.style.filter = 'grayscale(1) invert(1) hue-rotate(180deg)';
+        } else {
+          // Dark mode is inactive
+          googleMapsIframe.style.filter = 'none'; // Reset to default
+        }
+    }
+    if (AdobeDCview !== null){
+        if (theme == "dark") {
+          // Dark mode is active
+          AdobeDCview.style.filter = 'grayscale(1) invert(1) hue-rotate(180deg)';
+        } else {
+          // Dark mode is inactive
+          AdobeDCview.style.filter = 'none'; // Reset to default
+        }
     }
     });  
 });
