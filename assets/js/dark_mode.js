657cc1c4c98c733b4569f05374c2b5e49d82746f
arturavakyan <152524925+arturavakyan@users.noreply.github.com>
2023-12-01 22:59:15 +0100

    Update dark_mode.js

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     const mode_toggle = document.getElementById("light-toggle");
     const googleMapsIframe = document.getElementById('google-maps-iframe');
-
+    const AdobeDCview = document.getElementById('adobe-dc-view');
     mode_toggle.addEventListener("click", function() {
         toggleTheme(localStorage.getItem("theme"));
     theme=localStorage.getItem("theme")
     if (theme == "dark") {
       // Dark mode is active
       googleMapsIframe.style.filter = 'grayscale(1) invert(1) hue-rotate(180deg)';
+      AdobeDCview.style.filter = 'grayscale(1) invert(1) hue-rotate(180deg)';
     } else {
       // Dark mode is inactive
       googleMapsIframe.style.filter = 'none'; // Reset to default
+      AdobeDCview.style.filter = 'none'; // Reset to default
     }
     });  
 });
