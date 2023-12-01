49c0d1c84f7f9c6bc7df193fa9fe275cb4e271e6
arturavakyan <152524925+arturavakyan@users.noreply.github.com>
2023-12-01 23:22:26 +0100

    Update dark_mode.js

--- assets/js/dark_mode.js
+++ assets/js/dark_mode.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function() {
     if (AdobeDCview !== null){
         if (theme == "dark") {
           // Dark mode is active
-          AdobeDCview.style.filter = 'hue-rotate(180deg)';
+          AdobeDCview.style.filter = 'invert(1)';
         } else {
           // Dark mode is inactive
           AdobeDCview.style.filter = 'none'; // Reset to default
